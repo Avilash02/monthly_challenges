3e32cdf26c0c982b3e490e795114ed0c53eddef9
Avilash02 <avilashachar100@gmail.com>
2025-03-18 16:09:19 +0530

    add sumanth to view

--- greetings/views.py
+++ greetings/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello World')
+# def index(request):
+#     return HttpResponse('Hello World')
+
+def index1(request):
+    return HttpResponse('sumanth')
