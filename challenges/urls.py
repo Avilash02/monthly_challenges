c9e2010e77029e44fd9a966480ea0c55c66a1a76
Avilash02 <avilashachar100@gmail.com>
2025-03-21 10:32:32 +0530

    24. Dynamic Path segments & Captured Values

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("january",views.january),
-    path("february",views.february),
-    
+   path('<str:month>',views.monthly_challenge), # <str:month> is a path converter that captures the month from the URL and passes it to the view as a string.
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def january(request):
-    return HttpResponse("This Works!")
-
-def february(request):
-    return HttpResponse("This is february")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "January is the first month of the year."
+    elif month == 'february':
+        challenge_text = "February is the second month of the year."
+    elif month == 'march':
+        challenge_text = "March is the third month of the year."
+    elif month == 'april':
+        challenge_text = "April is the fourth month of the year."
+    elif month == 'may':
+        challenge_text = "May is the fifth month of the year."
+    return HttpResponse(challenge_text)
