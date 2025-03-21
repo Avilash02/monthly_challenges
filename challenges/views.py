455793c7c7d02c9e98a279e309ca1ddacc786f4e
Avilash02 <avilashachar100@gmail.com>
2025-03-21 10:06:19 +0530

    22. Created First View & URL

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def index(request):
+def january(request):
     return HttpResponse("This Works!")
+
+def february(request):
+    return HttpResponse("This is february")
